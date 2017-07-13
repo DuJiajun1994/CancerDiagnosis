79550aa85722dfbdd4e7c535bf1873d472f7b4f7
dujiajun <dujiajun1994@163.com>
2017-07-13 16:15:11 +0800

    add model provider

---
+++ lib/model_provider.py
@@ -0,0 +1,13 @@
+from models.vgg16 import vgg16
+
+def get_model(model_name):
+    if model_name == 'vgg16':
+        return vgg16
+    else:
+        raise Exception('model {} is not existed'.format(model_name))
+
+if __name__ == '__main__':
+    names = ['vgg16', 'sdfg']
+    for model_name in names:
+        print('model name: {}\n'.format(model_name))
+        model = get_model(model_name)
