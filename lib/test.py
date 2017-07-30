d74d9aff8f07f9700f4b2104c6e2ad3f5f99b2cb
dujiajun <dujiajun1994@163.com>
2017-07-30 07:44:40 -0700

    solve bug

--- lib/test.py
+++ lib/test.py
@@ -9,38 +9,31 @@ print(sys.path)
 import tensorflow as tf
 import argparse
 import sys
-from model_provider import get_model
 from data_provider import DataProvider
 from config_provider import get_config
 from paths import Paths
 
 
-def get_trained_model_path(model_name):
-    model_path = os.path.join(Paths.output_path, '{}.ckpt'.format(model_name))
-    assert os.path.exists(model_path), \
-        'trained model {} is not existed'.format(model_path)
-    return model_path
-
-
-def test_model(model_name, data_name, cfg_name, trained_model_name):
+def test_model(data_name, cfg_name, trained_model_name):
     cfg = get_config(cfg_name)
     print('Config:')
     print(cfg)
-    model = get_model(model_name)
     input_data = DataProvider(data_name)
 
-    x = tf.placeholder(tf.float32, shape=[cfg.batch_size, cfg.image_height, cfg.image_width, 3])  # images
-    y = tf.placeholder(tf.int64, shape=[cfg.batch_size])  # labels: 0, not cancer; 1, has cancer
-    predicts = model(x)
-    correct_predict = tf.equal(tf.argmax(predicts, 1), y)
-    accuracy = tf.reduce_mean(tf.cast(correct_predict, tf.float32))
-
-    saver = tf.train.Saver()
+    meta_path = os.path.join(Paths.output_path, '{}.ckpt.meta'.format(trained_model_name))
+    assert os.path.exists(meta_path), \
+        '{} is not existed'.format(meta_path)
+    saver = tf.train.import_meta_graph(meta_path)
 
     with tf.Session() as sess:
         # Load trained model
-        model_path = get_trained_model_path(trained_model_name)
-        saver.restore(sess, model_path)
+        saver.restore(sess, tf.train.latest_checkpoint('./'))
+
+        graph = tf.get_default_graph()
+        x = graph.get_tensor_by_name("x:0")
+        y = graph.get_tensor_by_name("y:0")
+        accuracy = graph.get_tensor_by_name("accuracy:0")
+        predicts = graph.get_tensor_by_name("predicts:0")
 
         test_accuracy = 0.
         test_num = int(input_data.test_size / cfg.batch_size)
@@ -60,16 +53,13 @@ def parse_args():
     Parse input arguments
     """
     parser = argparse.ArgumentParser(description='Train Cancer Diagnosis Network')
-    parser.add_argument('--net', dest='model_name',
-                        help='net to use',
-                        default='vgg16', type=str)
     parser.add_argument('--data', dest='data_name',
                         help='data to use',
                         default='', type=str)
     parser.add_argument('--cfg', dest='cfg_name',
                         help='train&test config to use',
                         default='', type=str)
-    parser.add_argument('--var', dest='trained_model_name',
+    parser.add_argument('--net', dest='trained_model_name',
                         help='trained model to use',
                         default='', type=str)
     if len(sys.argv) == 1:
@@ -82,4 +72,4 @@ if __name__ == '__main__':
     args = parse_args()
     print('Called with args:')
     print(args)
-    test_model(args.model_name, args.data_name, args.cfg_name, args.trained_model_name)
+    test_model(args.data_name, args.cfg_name, args.trained_model_name)
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Usage:
-# bash scripts/test.sh GPU_ID NET DATA CONFIG VAR
+# bash scripts/test.sh GPU_ID NET DATA CONFIG
 #
 # Example:
-# bash scripts/test.sh 0 vgg16 40 cfg1 VAR
+# bash scripts/test.sh 0 400_vgg16_10000 400 cfg1
 
 set -x
 set -e
@@ -14,11 +14,10 @@ GPU_ID=$1
 NET=$2
 DATA=$3
 CONFIG=$4
-VAR=$5
 
-LOG="logs/cancer_diagnosis_${VAR}_`date +'%Y_%m_%d_%H_%M_%S'`.txt"
+LOG="logs/cancer_diagnosis_${NET}_`date +'%Y_%m_%d_%H_%M_%S'`.txt"
 exec &> >(tee -a "$LOG")
 echo "Logging output to ${LOG}"
 
 export CUDA_VISIBLE_DEVICES=${GPU_ID}
-time python lib/test.py --net ${NET} --data ${DATA} --cfg ${CONFIG} --var ${VAR}
+time python lib/test.py --net ${NET} --data ${DATA} --cfg ${CONFIG}
